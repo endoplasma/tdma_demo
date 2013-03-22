b1664342a578f2bf3423897381b00a84d8dd082b
Philipp Spliethoff <philipp.spliethoff@udo.edu>
2013-03-22 11:16:14 +0100

    moep

--- contiki/radios/rf231_slotted/rf231_slotted.h
+++ contiki/radios/rf231_slotted/rf231_slotted.h
@@ -9,8 +9,10 @@
 /*============================ EXTERN DEFINITIONS ============================*/
 extern const struct radio_driver rf231_slotted_driver;
 
-#define NUM_PERIODS           8        /** The number of Periods used to calculate the average. User only powers of 2 */
-#define NUM_PERIODS_BASE      3        /** log2(NUM_PERIOD) used for fast division */
+#define NUM_PERIODS           8            /** The number of Periods used to calculate the average. User only powers of 2 */
+#define NUM_PERIODS_BASE      3            /** log2(NUM_PERIOD) used for fast division */
+ädefine PERIOD_BUFFER_LENGTH  8            /** length of the period buffer power of 2 */
+#define PERIOD_BUFFER_MASK    NUM_PERIOD-1 /** Mask for fast Buffer boundary wrapping */
 
 
 /*============================ PROCESS EVENTS ================================*/
